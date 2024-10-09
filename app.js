be33968cf9525a7c567ea3fae39eeab5215d584a
S4chan <heineken1204@gmail.com>
2024-10-09 16:38:31 +0800

    feat: add listing page

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
+const restaurants = require("./public/jsons/restaurant.json").results;
+const BASE_IMG_URL =
+  "https://assets-lighthouse.s3.amazonaws.com/uploads/image/file/";
 
-app.use(express.static("public"));
+//元寫法 app.use(express.static("public"));
+app.use("/public", express.static("public"));
+app.use("/javascripts", express.static("public/javascripts"));
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
@@ -13,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/restaurants", (req, res) => {
-  res.render("index");
+  res.render("index", { restaurants, BASE_IMG_URL });
 });
 
 app.get("/restaurant/:id", (req, res) => {
